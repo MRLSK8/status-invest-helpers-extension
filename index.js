13a9504ff5ac42a247c5ef9b927e742007dd9414
Marcelo Lima <jmdl.k8@gmail.com>
2022-03-28 12:11:39 -0300

    refactoring function which shows my earning

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ function getEarningValue() {
   const totalValueField = document.querySelector("div.mr-3 > strong.d-flex > span.sensitive-field");
   const currentValueField = document.querySelector("div > strong.d-flex > span.sensitive-field");
   const currentValueDiv = document.querySelector("div > div.card.p-1.pl-3.pr-3.white-text.bg-main-green.d-flex.d-sm-block.justify-between.align-items-center.border-0");
-  
+
   const totalValueCurrency = currencyStringValueToNumber(totalValueField.innerText);
   const currentValueCurrency = currencyStringValueToNumber(currentValueField.innerText);
-  
+
+  if (!totalValueCurrency || !currentValueCurrency) return;
+
   const earningValue = currentValueCurrency - totalValueCurrency;
-  
+
   const span = document.createElement('span');
-  
+
   span.classList.add("fw-700");
 
   if (earningValue < 0) {
     span.classList.add("text-main-secondary");
   }
-  
+
   span.setAttribute('data-item', "total_F");
 
   span.innerText = "R$ " + numberToCurrencyStringValue(earningValue.toFixed(2));
@@ -40,8 +42,15 @@ function getEarningValue() {
   }
 }
 
-setTimeout(() => {
-  getEarningValue();
-}, 2500);
+const main = () => {
+  const MILISECONDS = 1000;
+
+  setTimeout(() => {
+    getEarningValue();
+    setInterval(getEarningValue, 15 * MILISECONDS);
+  }, 3 * MILISECONDS);
+};
+
+main();
 
 
